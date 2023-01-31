75c576b56396e94c50a462190dc614da559c4727
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2023-01-31 16:20:38 +0800

    add script for ed25519 test

--- jacob_ds_btc_ok/admin/clearAllMpc.sh
+++ jacob_ds_btc_ok/admin/clearAllMpc.sh
@@ -1,9 +1,9 @@
 
 ps -ef | grep schnorrmpc | grep -v grep | grep -v attach |awk '{print $2}'| xargs kill -9 
 
-find ../nodes -type d -name "schnorrmpc" | xargs rm -rf
+#find ../nodes -type d -name "schnorrmpc" | xargs rm -rf
 
-find ../nodes -type d -name "storeman" | xargs rm -rf
+#find ../nodes -type d -name "storeman" | xargs rm -rf
 
 #find ~/jacob -type d -name "schnorrmpc" | xargs rm -rf 
 
