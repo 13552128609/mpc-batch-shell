8b0960b6ee5f98897b7841cc2d5cc57c5b145773
Your Name <you@example.com>
2023-11-22 08:27:38 +0000

    add peersTestnetDocker.sh

---
+++ jacob_ds_btc_ok/admin/peersTestnetDocker.sh
@@ -0,0 +1,18 @@
+ROOTPATH="/home/qjia/mpc-batch-shell/jacob_ds_btc_ok/"
+for container in $(docker ps | grep openstoreman |grep -v openstoreman9 | awk '{print $NF}')
+do
+	echo && echo " ********************************************************* "
+	echo $container
+
+	cmd="sudo docker exec  -i $container /osm/schnorrmpc/bin/schnorrmpc attach /osm/schnorrmpc/data/gwan.ipc < $ROOTPATH/cmd/peers.cmd"
+	echo $cmd
+	eval $cmd
+	echo && echo " ********************************************************* "
+done
+
+        container='openstoreman_bootnode'
+	echo && echo " ********************************************************* "
+	echo $container
+	cmd="sudo docker exec  -i $container /osm/schnorrmpc/bin/schnorrmpc attach /osm/schnorrmpc/data/gwan.ipc < $ROOTPATH/cmd/peers.cmd"
+	eval $cmd
+	echo && echo " ********************************************************* "
