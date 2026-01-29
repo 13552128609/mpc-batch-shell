e6459848c2680c7d590df91e0d865246b3ff35e2
jacob <2627302231@qq.com>
2026-01-29 16:36:23 +0800

    update for tolerance

--- jacob_ds_btc_ok/test/js/sign.js
+++ jacob_ds_btc_ok/test/js/sign.js
@@ -1,4 +1,4 @@
-const ipc = "/home/jacob/mpc-batch-shell/jacob_ds_btc_ok/nodes/n01/data/mpc.ipc"
+const ipc = "/home/jacob/wanchain/mpc-batch-shell/jacob_ds_btc_ok/nodes/n01/data/mpc.ipc"
 
 const Web3 = require("web3")
 const conf = require("./data").conf
@@ -25,60 +25,63 @@ async function main() {
 	NUM = parseInt(argv[2])
 	BATCH=parseInt(argv[3])
 
-    let signDataS = []
-    for (let i = 0; i < NUM; i++) {
-        if (parseInt(i) === 0) {
-            hashData = conf.hashData
-        } else {
-            //hashData = web3.sha3(hashData, {encoding: 'hex'}); //todo should restore
-            hashData = conf.hashData
-        }
-        let signData = {
-            pk: conf.pk,
-            curve: conf.curve,
-            alg: conf.alg,
-            hashData: [hashData],
-            rawData: [conf.rawData],
-            extern: conf.extern,
-        }
-        signDataS.push(signData)
-        //console.log(signDataS.length)
-    }
+	let signDataS = []
+	for (let i = 0; i < NUM; i++) {
+		if (parseInt(i) === 0) {
+			hashData = conf.hashData
+		} else {
+			//hashData = web3.sha3(hashData, {encoding: 'hex'}); //todo should restore
+			hashData = conf.hashData
+		}
+		let signData = {
+			pk: conf.pk,
+			curve: conf.curve,
+			alg: conf.alg,
+			hashData: [hashData],
+			rawData: [conf.rawData],
+			extern: conf.extern,
+		}
+		signDataS.push(signData)
+		//console.log(signDataS.length)
+	}
 
 	let allBegin=Date.now()
-    let remain = BATCH //todo 10
+	let remain = BATCH //todo 10
 	var signed = 0
-    while (parseInt(signDataS.length) != 0) {
-        for (let j = 0; j < remain; j++) {
-            let signDataTemp = {}
-            Object.assign(signDataTemp, signDataS.pop())
-            //console.log("signDataTemp", signDataTemp)
-            //console.log("web3.storeman", web3.storeman)
-            let begin=Date.now()
-            //web3.storeman.signByApprove(signDataTemp, async (err, result) => {
-            web3.storeman.signByApprove(signDataTemp, (err, result) => {
-		    signed++
-		    let end=Date.now()
-		    if(parseInt(signed) == parseInt(NUM)){
-    				console.log("lastNum",NUM,"allTime",(Date.now()-allBegin)/1000)
-		    }
-                console.log("signedNum",signed,"status", err==null?'success':err, "result", result==null?'null':result.ResultType,"incentive",result!=null?result.IncntData:'null',
-			"during",parseInt((end-begin)/1000),"begin",parseInt(begin/1000),"end",parseInt(end/1000),"beginDate",(new Date(begin)), "endDate",(new Date(end)))
-                remain = remain + 1
-            })
-        }
-        remain = 0
-        await sleep(100)
-    }
+	while (parseInt(signDataS.length) != 0) {
+		for (let j = 0; j < remain; j++) {
+			let signDataTemp = {}
+			Object.assign(signDataTemp, signDataS.pop())
+			//console.log("signDataTemp", signDataTemp)
+			//console.log("web3.storeman", web3.storeman)
+			let begin=Date.now()
+			//web3.storeman.signByApprove(signDataTemp, async (err, result) => {
+			web3.storeman.signByApprove(signDataTemp, (err, result) => {
+				signed++
+				let end=Date.now()
+				console.log(`signDataTemp ${signed}`,signDataTemp);
+				console.log(`result ${signed}`,result);
+				console.log("signedNum",signed,"status", err==null?'success':err, "result", result==null?'null':result.ResultType,"incentive",result!=null?result.IncntData:'null',
+					"during",parseInt((end-begin)/1000),"begin",parseInt(begin/1000),"end",parseInt(end/1000),"beginDate",(new Date(begin)), "endDate",(new Date(end)))
+				remain = remain + 1
+				if(parseInt(signed) == parseInt(NUM)){
+					console.log("lastNum",NUM,"allTime",(Date.now()-allBegin)/1000)
+				}
+
+			})
+		}
+		remain = 0
+		await sleep(100)
+	}
 }
 
 
 async function sleep(time) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve();
-        }, time);
-    });
+	return new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			resolve();
+		}, time);
+	});
 }
 
 
